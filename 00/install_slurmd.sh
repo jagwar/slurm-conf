c485cb33c3dbac8b77a4b75c4ca9528d06442cb3
Guillaume Salou <g.salou@gmail.com>
2023-02-07 10:31:05 +0100

    Update install_slurmd.sh

--- 00/install_slurmd.sh
+++ 00/install_slurmd.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+!/bin/bash
 set -x
 set -e
 #needed on ubuntu2004
+apt update
 # apt install -y build-essential pkg-config libcurl4-openssl-dev libdbus-1-dev
-rm /usr/local/cuda
-ln -s /usr/local/cuda-11.7 /usr/local/cuda
+#rm /usr/local/cuda
+#ln -s /usr/local/cuda-11.7 /usr/local/cuda
 #Todo find what's this file: 888-slurm.sh
 #cp /admin/hosts/888-slurm.sh /etc/profile.d/
 
@@ -14,6 +15,7 @@ apt-get  -q -o DPkg::Lock::Timeout=240 install -y munge libmunge-dev hwloc libhw
   libpmix-dev librrd-dev bzip2 libjson-c-dev libhttp-parser-dev
 #we should probably get this from s3 and not hit their web servers
 adduser --quiet --system --group --uid 401 --no-create-home --home /nonexistent slurm || true
+#todo check slurm user
 SLURM_VER="slurm-22.05.5.tar.bz2"
 SLURM_URL=https://download.schedmd.com/slurm/${SLURM_VER}
 TEMP_DIR="/tmp/slurm_tmp"
@@ -35,8 +37,8 @@ systemctl restart munge.service
 cat >/usr/lib/systemd/system/slurmd.service <<EOF
 [Unit]
 Description=Slurm node daemon
-After=munge.service network.target remote-fs.target admin.mount fsx.mount opt-df.mount #use your own mounts here as precondition to start slurmd
-Wants=munge.service network.target remote-fs.target admin.mount fsx.mount opt-df.mount
+After=munge.service #use your own mounts here as precondition to start slurmd
+Wants=munge.service
 #ConditionPathExists=/opt/slurm/etc/slurm.conf
 Documentation=man:slurmd(8)
 [Service]
@@ -57,12 +59,3 @@ WantedBy=multi-user.target graphical.target
 EOF
 cp /usr/lib/systemd/system/slurmd.service /etc/systemd/system/
 
-# wrap sbatch with the custom one
-mv /opt/slurm/bin/sbatch /opt/slurm/sbin/sbatch
-mv /opt/slurm/bin/srun /opt/slurm/sbin/srun
-ln -s /admin/slurm/sbatch /opt/slurm/bin/sbatch
-ln -s /admin/slurm/sbatch /opt/slurm/bin/srun
-
-#block TOR nodes
-sleep $[$RANDOM % 60]s && /admin/config/ipset-tor-init.sh
-crontab /admin/hosts/crontab
