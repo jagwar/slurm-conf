e6f17c45b4f65318f0962cc51afe1f0597c93f9c
Guillaume Salou <g.salou@gmail.com>
2023-02-15 23:08:12 +0100

    Create update-nccl.sh

---
+++ 00/custom-scripts-and-configs/update-nccl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+set -e
+cd /opt
+sudo git clone https://github.com/NVIDIA/nccl.git
+cd nccl
+sudo git checkout inc_nsteps
+sudo make -j src.build CUDA_HOME=/usr/local/cuda-${CUSTOM_CUDA_VERSION} NVCC_GENCODE='-gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75 -gencode=arch=compute_80,code=sm_80'
+echo -e '#!/bin/sh\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nccl/build/lib\nexport NCCL_PROTO=simple' | sudo tee /etc/profile.d/nccl.sh
